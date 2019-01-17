bc943e1eb306c4f73d4f05bc90e207776dadf734
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2019-01-17 23:07:46 +0100

    adding showmovie

---
+++ unity/showmovie/Assets/ClickOnMe.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ClickOnMe : MonoBehaviour {
+
+    public VideoPlayer player;
+    public VideoClip clip;
+    public AudioSource audioSource;
+
+
+    private void Start()
+    {
+        player.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        player.SetTargetAudioSource(0, audioSource);
+        player.source = VideoSource.VideoClip;
+        player.clip = clip;
+    }
+
+    bool active = false;
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        if (!active)
+        {
+            player.Play();
+            Debug.Log("ON!");
+        } else
+        {
+            player.Stop();
+            Debug.Log("OFF");
+        }
+        active = !active;
+    }
+
+   
+}
